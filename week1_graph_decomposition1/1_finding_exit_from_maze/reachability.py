db0b4385e71ce13e42a3547009d40bb06be163fa
beetee17 <brandon.thio@yahoo.com.sg>
2021-04-25 20:40:54 +0800

    init

--- week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
+++ week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
@@ -2,20 +2,74 @@
 
 import sys
 
-def reach(adj, x, y):
-    #write your code here
-    return 0
+class Vertex():
+    def __init__(self, index):
+        self.index = index
+        self.neighbours = {}
+        self.visited = False
+        self.component = None
+
+    def addNeighbour(self, w):
+        self.neighbours.update({w : None})
+
+class Graph():
+    def __init__(self, edges, vertices):
+        self.edges = edges
+        self.vertices = vertices
+        self.components = 0
+    
+    def explore(self, V):
+
+        V.visited = True
+        V.component = self.components
+        for v, w in self.edges:
+            if V == w:
+                if not v.visited:
+                    self.explore(v)
+            elif V == v:
+                if not w.visited:
+                    self.explore(w)
+            
+
+    def DFS(self):
+    
+        for v in self.vertices:
+            if not v.visited:
+                self.explore(v)
+                self.components += 1
+
+
+
+
+def reach(graph, x, y):
+    graph.DFS()
+
+    return 1 if graph.vertices[x].component == graph.vertices[y].component else 0
+    
 
 if __name__ == '__main__':
     input = sys.stdin.read()
+
     data = list(map(int, input.split()))
+
     n, m = data[0:2]
     data = data[2:]
+
+    vertices = [Vertex(i) for i in range(n)]
+
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+
     x, y = data[2 * m:]
-    adj = [[] for _ in range(n)]
+
+    adj = []
+
     x, y = x - 1, y - 1
+    
+    
     for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+     
+        adj.append([vertices[a-1], vertices[b-1]])
+
+    graph = Graph(adj, vertices)
+
+    print(reach(graph, x, y))
--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -2,20 +2,63 @@
 
 import sys
 
+class Vertex():
+    def __init__(self, index):
+        self.index = index
+        self.neighbours = {}
+        self.visited = False
+        self.component = None
 
-def number_of_components(adj):
-    result = 0
-    #write your code here
-    return result
+    def addNeighbour(self, w):
+        self.neighbours.update({w : None})
+
+class Graph():
+    def __init__(self, edges, vertices):
+        self.edges = edges
+        self.vertices = vertices
+        self.components = 0
+    
+    def explore(self, V):
+        
+        V.visited = True
+        V.component = self.components
+
+        for v, w in self.edges:
+            # find the edges of the vertex in question and explore its neighbour
+            if V == w:
+                if not v.visited:
+                    self.explore(v)
+            elif V == v:
+                if not w.visited:
+                    self.explore(w)
+            
+
+    def DFS(self):
+    
+        for v in self.vertices:
+            if not v.visited:
+                # explore each component recursively
+                self.explore(v)
+                self.components += 1
+
+def number_of_components(graph):
+    
+    graph.DFS()
+    return graph.components
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
+
+    vertices = [Vertex(i) for i in range(n)]
+
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
+
+    adj = []
     for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(number_of_components(adj))
+        adj.append([vertices[a-1], vertices[b-1]])
+
+    graph = Graph(adj, vertices)
+    print(number_of_components(graph))
